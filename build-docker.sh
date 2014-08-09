9e46787d349beb498e879cd5c8bb4cc11537f450
Joe Lombrozo <joe@djeebus.net>
2014-08-09 11:33:25 -0700

    Fixed nginx config (maybe?) Added cert generation check when building the image.

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+HOST_NAME=htpc-downloader.local
+
+
+BASE_FILE=nginx/ssl
+
+if [ ! -f $BASE_FILE.key ] || [ ! -f $BASE_FILE.crt ]
+then
+	openssl req -new \
+        	-newkey rsa:4096 \
+	        -days 365 \
+	        -nodes -x509 \
+	        -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=$HOST_NAME" \
+	        -keyout $BASE_FILE.key \
+	        -out $BASE_FILE.crt
+fi
+
 docker build -t djeebus/htpc-container .
